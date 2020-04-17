b18b78f5de805f5a02ebb122bc756ef38ebda75a
sudhayakumar3 <sudhayakumar3@gatech.edu>
2020-04-16 20:37:51 -0400

    code cleaner

--- main_plan.py
+++ main_plan.py
@@ -1,38 +1,49 @@
+######################################
+#Imports
+######################################
+#general imports
 import math
 import matplotlib.pyplot as plt
 import numpy as np
 
+#Imports needed for path planning
 from a_star import AStarPlanner
 
-show_animation = True
-
+#Imports needed for arm kinematics
 from NLinkArm import NLinkArm
 from kinematics_main import inverse_kinematics, forward_kinematics, jacobian_inverse, distance_to_goal, ang_diff
 
-####forward kinematics stuff
-Kp = 1 #0.2
-dt = 0.1
-N_LINKS = 3
-N_ITERATIONS = 10000
+######################################
+#INPUTS 
+######################################
+#for A* path planning
+show_animation = True
+grid_size = 2.0  # [m]
+robot_radius = 1.0  # [m]
 
-# States
+# start and goal position
+robotStartPose = (10.0,10.0)  # (x,y) [m]
+robotEndPose = (48.0, 48.0)  # (x,y) [m]
+objectPose = (50.0, 50.0)
+
+#needed for arm kinematics
+N_LINKS = 3
+link_lengths = [1] * N_LINKS
+joint_angles = np.array([0] * N_LINKS)  
+N_ITERATIONS = 10000
 WAIT_FOR_NEW_GOAL = 1
 MOVING_TO_GOAL = 2
+#Kp = 1 #0.2
+#dt = 0.1
 
-def main():
-    print(__file__ + " start!!")
 
-    # start and goal position
-    sx = 10.0  # [m]
-    sy = 10.0  # [m]
-    gx = 48.0  # [m]
-    gy = 48.0  # [m]
-    obj_x = 50.0  # [m]
-    obj_y = 50.0  # [m]
 
-    grid_size = 2.0  # [m]
-    robot_radius = 1.0  # [m]
 
+
+######################################
+#functions
+######################################
+def generateObstacleLocationMap():
     # set obstable positions
     ox, oy = [], []
     for i in range(-10, 60):
@@ -53,31 +64,56 @@ def main():
     for i in range(0, 40):
         ox.append(40.0)
         oy.append(60.0 - i)
+    return ox,oy
 
+##
+#Gets x and y pixel positions containing obstacles. obstacles include the bounding wall
+##
+def generateMapPlotWithRobot(xPositionOfObstacles,yPositionOfObstacles, robotStartPose, robotEndPose, objectPose):
     if show_animation:  # pragma: no cover
-        plt.plot(ox, oy, ".k")
-        plt.plot(sx, sy, "og")
-        plt.plot(gx, gy, "xb")
-        plt.plot(obj_x, obj_y, "xr")
+        plt.plot(xPositionOfObstacles, yPositionOfObstacles, ".k")
+        plt.plot(robotStartPose[0], robotStartPose[1], "og")
+        plt.plot(robotEndPose[0], robotEndPose[1], "xb")
+        plt.plot(objectPose[0], objectPose[1], "xr")
         plt.grid(True)
         plt.axis("equal")
 
-    a_star = AStarPlanner(ox, oy, grid_size, robot_radius)
-    rx, ry = a_star.planning(sx, sy, gx, gy)
-    print("rx = ", rx)
+def main():
+    #initial robot parameters
+  
 
+    #Gets x and y positions of image coordinates containing obstacles. obstacles include the bounding wall
+    xPositionOfObstacles, yPositionOfObstacles = generateObstacleLocationMap()
+    
+    #plot the map with the obstacles and robot
+    generateMapPlotWithRobot(xPositionOfObstacles, yPositionOfObstacles, robotStartPose, robotEndPose, objectPose)
+
+    #Generate path to goal
+    sx = robotStartPose[0]
+    sy = robotStartPose[1]
+    gx = robotEndPose[0]
+    gy = robotEndPose[1]
+    obj_x = objectPose[0]
+    obj_y = objectPose[1]
+
+    a_star = AStarPlanner(xPositionOfObstacles, yPositionOfObstacles, grid_size, robot_radius)
+    rx, ry = a_star.planning(sx, sy, gx, gy)
+
+    #Show calculated path
     if show_animation:  # pragma: no cover
         plt.plot(rx, ry, "-r")
         plt.show()
-        
-    start_kin_x = rx[0]
-    start_kin_y = ry[0]
+    
+    #animate robot to move along desired trajectory
+    #TO DO: 
+
+
     goal_x_kin, goal_y_kin = (obj_x-gx), (obj_y-gy)
     
-    #do inverse kinematics
+    #Calculate arm pose to grab object
+    #reorientArmToGrabObject()
     
-    link_lengths = [1] * N_LINKS
-    joint_angles = np.array([0] * N_LINKS)
+
     goal_pos = goal_x_kin, goal_y_kin
     
     arm = NLinkArm(link_lengths, joint_angles, goal_pos, show_animation)
@@ -95,19 +131,29 @@ def main():
     print("distance = ", distance)
     print("state = ", state)
     print("solution_found = ", solution_found)
-    if state is WAIT_FOR_NEW_GOAL:
     
-        if distance > 0.1 and not solution_found:
-            joint_goal_angles, solution_found = inverse_kinematics(
-                link_lengths, joint_angles, goal_pos)
-            if not solution_found:
-                print("Solution could not be found.")
-    
-            elif solution_found:
+    if distance > 0.1 and not solution_found:
+        joint_goal_angles, solution_found = inverse_kinematics(
+            link_lengths, joint_angles, goal_pos)
+        if not solution_found:
+            print("Solution could not be found.")
+        elif solution_found:
                 state = MOVING_TO_GOAL
-    #joint_angles = joint_angles + Kp * \
-                #ang_diff(joint_goal_angles, joint_angles) * dt
     arm.update_joints(joint_goal_angles)
+
+#    if state is WAIT_FOR_NEW_GOAL:
+#    
+#        if distance > 0.1 and not solution_found:
+#            joint_goal_angles, solution_found = inverse_kinematics(
+#                link_lengths, joint_angles, goal_pos)
+#            if not solution_found:
+#                print("Solution could not be found.")
+#    
+#            elif solution_found:
+#                state = MOVING_TO_GOAL
+#    #joint_angles = joint_angles + Kp * \
+#                #ang_diff(joint_goal_angles, joint_angles) * dt
+#    arm.update_joints(joint_goal_angles)
             
 #    if state is MOVING_TO_GOAL:
 #        if distance > 0.1 and all(old_goal == goal_pos):
